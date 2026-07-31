3d1927db5555a30dc474870f2553b967d732426a
Aditya2584 <bestrong2584@gmail.com>
2026-07-31 12:19:45 +0530

    Reconnection

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 
-// Routes import
+// Route import
 const authRouter = require('./routes/auth.routes');
 const platformRouter = require('./routes/platform.routes');
 const submissionRouter = require('./routes/submission.routes');
